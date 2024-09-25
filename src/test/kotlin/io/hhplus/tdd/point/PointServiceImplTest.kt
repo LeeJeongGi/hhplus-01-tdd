1003dee585cc26e69edeb74360b5a258a9abc069
LeeJeongGi <apple4rhk@naver.com>
2024-09-25 23:03:57 +0900

    test : 한 유저에게 여러건의 요청이 비동기적으로 들어 왔을 경우 순차적으로 수행되는지 검증 테스트 추가

--- src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
@@ -3,28 +3,30 @@ package io.hhplus.tdd.point
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.dto.PointRequest
+import io.hhplus.tdd.point.handler.UserLockManager
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
-import java.util.concurrent.CountDownLatch
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest
 class PointServiceImplTest {
 
     private lateinit var pointHistoryTable: PointHistoryTable
     private lateinit var userPointTable: UserPointTable
-    private lateinit var pointServiceImpl: PointServiceImpl
+    private lateinit var pointService: PointServiceImpl
 
     @BeforeEach
     fun setUp() {
         userPointTable = UserPointTable()
         pointHistoryTable = PointHistoryTable()
 
-        pointServiceImpl = PointServiceImpl(pointHistoryTable, userPointTable)
+        pointService = PointServiceImpl(pointHistoryTable, userPointTable, userLockManager = UserLockManager())
     }
 
     @Test
@@ -34,7 +36,7 @@ class PointServiceImplTest {
         userPointTable.insertOrUpdate(1L, 300L)
 
         // when
-        val userPoint = pointServiceImpl.getUserPoint(1L)
+        val userPoint = pointService.getUserPoint(1L)
 
         // then
         assertThat(userPoint).isNotNull
@@ -46,7 +48,7 @@ class PointServiceImplTest {
     @DisplayName("저장이 아직 안된 유저의 포인트를 조회")
     fun getDoNotSaveUserPointTest() {
         // when
-        val userPoint = pointServiceImpl.getUserPoint(1L)
+        val userPoint = pointService.getUserPoint(1L)
 
         // then
         assertThat(userPoint).isNotNull
@@ -63,7 +65,7 @@ class PointServiceImplTest {
         val history3 = pointHistoryTable.insert(2L, 500L, TransactionType.USE, System.currentTimeMillis())
 
         // when
-        val userPointHistory = pointServiceImpl.getUserPointHistory(2L)
+        val userPointHistory = pointService.getUserPointHistory(2L)
 
         // then
         assertThat(userPointHistory).hasSize(3)
@@ -88,7 +90,7 @@ class PointServiceImplTest {
         val pointRequest = PointRequest(1L, 400L)
 
         // when
-        val saveUserPoint = pointServiceImpl.savePoint(pointRequest)
+        val saveUserPoint = pointService.savePoint(pointRequest)
 
         // then
         assertThat(saveUserPoint.id).isEqualTo(pointRequest.userId)
@@ -100,10 +102,10 @@ class PointServiceImplTest {
     fun savePlusPointTest() {
         // given
         val pointRequest = PointRequest(1L, 400L)
-        val saveUserPoint = pointServiceImpl.savePoint(pointRequest)
+        val saveUserPoint = pointService.savePoint(pointRequest)
 
         // when
-        val curUserPoint = pointServiceImpl.savePoint(PointRequest(saveUserPoint.id, amount = saveUserPoint.point + 100L))
+        val curUserPoint = pointService.savePoint(PointRequest(saveUserPoint.id, amount = saveUserPoint.point + 100L))
 
         // then
         assertThat(curUserPoint.id).isEqualTo(pointRequest.userId)
@@ -118,7 +120,7 @@ class PointServiceImplTest {
 
         // when & then
         val message = assertThrows<IllegalArgumentException> {
-            pointServiceImpl.savePoint(PointRequest(1L, 1001L))
+            pointService.savePoint(PointRequest(1L, 1001L))
         }.message
 
         assertThat(message).isEqualTo("최대 포인트는 1000000원입니다. 현재 포인트: 999000, 요청한 포인트: 1001")
@@ -132,7 +134,7 @@ class PointServiceImplTest {
 
         // when & then
         val message = assertThrows<IllegalArgumentException> {
-            pointServiceImpl.usePoint(PointRequest(1L, 1000L))
+            pointService.usePoint(PointRequest(1L, 1000L))
         }.message
 
         assertThat(message).isEqualTo("사용할 수 있는 포인트가 부족합니다. 현재 포인트: 500, 요청한 포인트: 1000")
@@ -141,30 +143,43 @@ class PointServiceImplTest {
     @Test
     @DisplayName("포인트 충전과 사용이 순차적으로 처리되는지 테스트")
     fun executeSequentiallyOnConcurrentUsage() {
-
         // given
-        val threadCount = 2
-        val latch = CountDownLatch(threadCount)
-        val executor = Executors.newFixedThreadPool(threadCount)
+        val userId = 1L
+        val initialAmount = 1000L
+        val requestAmount1 = 500L
+        val requestAmount2 = 300L
+
+        // 포인트 초기 상태 설정
+        pointService.savePoint(PointRequest(userId, initialAmount))
+
+        val executor = Executors.newFixedThreadPool(2)
+        val thread1 = CompletableFuture.supplyAsync({
+            pointService.savePoint(PointRequest(userId, requestAmount1))
+        }, executor)
+
+        val thread2 = CompletableFuture.supplyAsync({
+            pointService.usePoint(PointRequest(userId, requestAmount2))
+        }, executor)
 
         // when
-        repeat(threadCount) { i ->
-            executor.execute {
-                try {
-                    pointServiceImpl.savePoint(PointRequest(3L, 1000L))
-                    pointServiceImpl.usePoint(PointRequest(3L, 500L))
-                } finally {
-                    latch.countDown()
-                }
-            }
-        }
-
-        latch.await()
+        CompletableFuture.allOf(thread1, thread2).join()
 
         // then
-        assertThat(pointHistoryTable.selectAllByUserId(3L).size).isEqualTo(threadCount * 2)
+        val finalPoint = pointService.getUserPoint(userId)
+        assertThat(finalPoint.point).isEqualTo(initialAmount + requestAmount1 - requestAmount2)
 
-        val finalPoint = pointServiceImpl.getUserPoint(3L)
-        assertThat(finalPoint.point).isEqualTo(1000L)
+        val userPointHistory = pointService.getUserPointHistory(userId)
+        assertThat(userPointHistory[0].amount).isEqualTo(1000L)
+        assertThat(userPointHistory[0].type).isEqualTo(TransactionType.CHARGE)
+
+        assertThat(userPointHistory[1].amount).isEqualTo(500L)
+        assertThat(userPointHistory[1].type).isEqualTo(TransactionType.CHARGE)
+
+        assertThat(userPointHistory[2].amount).isEqualTo(300L)
+        assertThat(userPointHistory[2].type).isEqualTo(TransactionType.USE)
+
+        // 실행 중인 스레드 풀 종료
+        executor.shutdown()
+        executor.awaitTermination(5, TimeUnit.SECONDS)
     }
 }
